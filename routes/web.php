a6792b761c0484a87f856822623564624ff174c4
Lan3 <151654151+Zer0shiroo@users.noreply.github.com>
2025-03-06 22:43:56 +0100

    Second commit

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return redirect()->route('oficinas.index'); // Redirige automáticamente a la página de oficinas
+    return redirect()->route('oficinas.index'); 
 });
 
 Route::resource('oficinas', OficinaController::class);
